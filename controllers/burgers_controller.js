875714a2402e85c4eec79f90444442f115d11abc
Ryan Brutocao <ryanbrutocao@outlook.com>
2019-09-20 15:23:25 -0600

    have all functionality working... need to work on CSS and README

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,10 @@
 var express = require("express");
 
-var burger = require("../models/burger");
 var router = express.Router();
+
+var burger = require("../models/burger");
+
+
 
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
@@ -10,6 +13,47 @@ router.get("/", function(req, res) {
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req,res){
+  burger.insertOne([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+
+    res.json({id: result.insertId})
+  })
+})
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.deleteOne(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
 });
 
